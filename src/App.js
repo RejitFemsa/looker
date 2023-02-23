0301135ccc8557343ba2fbac3394880918922747
RejitFemsa <juan.fuentes@oxxo.com>
2023-02-23 13:07:22 -0600

    another dash

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <iframe
+          title="looker2"
+          src="https://digitalnonprodlooker.cloud.looker.com/embed/dashboards/95"
+          width="80%"
+          height="500px"
+          frameBorder="0"
+        />
+        <iframe
           title="looker"
           src="https://digitalnonprodlooker.cloud.looker.com/embed/dashboards/87?Tipo+de+Cliente%E2%80%8B=&Fecha+Inicio%E2%80%8B+-+Fecha+Fin%E2%80%8B=2022%2F10%2F01+to+2022%2F11%2F01"
           width="80%"
